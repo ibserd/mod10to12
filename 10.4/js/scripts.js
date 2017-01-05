ca403ce7395aa4921892b6d5ebdd754946f7d81c
ibserd <ibserd@gmail.com>
2017-01-05 20:04:19 +0100

    first commit

---
+++ 10.4/js/scripts.js
@@ -0,0 +1,17 @@
+function Telefon(marka, cena, kolor) {
+  this.marka = marka;
+  this.cena = cena;
+  this.kolor = kolor;
+}
+
+Telefon.prototype.printInfo = function() {
+  console.log('Marka tego telefonu to ' + this.marka + ', kolor to ' + this.kolor + ', a jego cena wynosi ' + this.cena + 'zł.')
+}
+
+var iphone6s = new Telefon("Apple", 3500, "czarny"),
+    GalaxyS6 = new Telefon("Samsung", 2500, "czarny"),
+    P9 = new Telefon("Huwawei", 1100, "biały");
+
+P9.printInfo();
+GalaxyS6.printInfo();
+iphone6s.printinfo();
